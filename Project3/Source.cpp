d1d2de72fc17259f9b6dcf8338cdf199d1deb188
Tristan.M.Briceno <Tristan.M.Briceno@DESKTOP-2NT3M6V>
2022-10-13 09:03:33 +0200

    Add project files.

---
+++ Project3/Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+
+
+//float Gange(float x, float y)
+//{
+    //return x * y;
+//}
+
+bool isRunning = true;
+//void funksjon, her lager vi programmet vårt utenfor hovedkoden
+
+
+
+void basse()
+{
+    //datatyper
+
+
+    float fahrenheit;
+    float produkt;
+
+    //sett inn verdiene vi ønsker å bruke i regnestykket
+    float x = 2.2;
+    float y = 3.3;
+    produkt = x * y;
+
+
+
+
+    fahrenheit = produkt + 32 / 0.55;
+    cout << produkt;
+    cout << " Celsius\n";
+    cout << fahrenheit;
+    cout << " fahrenheit\n";
+    //printer svara
+
+}
+int main()
+{
+
+    if (isRunning = true)
+    {
+        //her setter vi inn void funksjonen vi lagde ovenfor
+        basse();
+        isRunning = false;
+    }
+
+
+
+
+    //float();
+        //{
+        //float p = Gange(6.21, 5.122);
+        //cout << p;
+        //cout << " Celsius\n";
+        //float svar = p + 32 / 0.55;
+        //cout << svar;
+        //cout << "Fahrenheit";
+        //}
+
+
+
+}
